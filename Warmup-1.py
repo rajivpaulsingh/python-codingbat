f6246076c142427777bbcc34a12a8612a5fc022b
Singh.Rajiv@bcg.com <Singh.Rajiv@bcg.com>
2021-11-18 15:45:56 -0800

    Added code for string-1

--- Warmup-1.py
+++ Warmup-1.py
@@ -63,7 +63,6 @@ We have a loud talking parrot. The "hour" parameter is the current hour time in 
 We are in trouble if the parrot is talking and the hour is before 7 or after 20. 
 Return True if we are in trouble.
 
-
 parrot_trouble(True, 6) → True
 parrot_trouble(True, 7) → False
 parrot_trouble(False, 6) → False
