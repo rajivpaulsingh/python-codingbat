37a1cfe950f80980152229e3cf0dc9d3e028c4a5
Singh.Rajiv@bcg.com <Singh.Rajiv@bcg.com>
2021-11-18 16:21:01 -0800

    Added code for warmup-2

--- List-1.py
+++ List-1.py
@@ -103,7 +103,6 @@ def max_end3(nums):
 
 # sum2
 """
-
 Given an array of ints, return the sum of the first 2 elements in the array. 
 If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
 
